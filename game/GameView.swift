dd4fd11047cadcafd8ea92984bdc8e06173fdc1e
zengyuhuei <zengyuhuei@gmail.com>
2019-05-12 22:51:28 +0800

    修改

--- game/GameView.swift
+++ game/GameView.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AudioToolbox
 var timer: Timer?
+var gameTimer: Timer?
+
+var down: Timer?
 var oldTime = "00"
-let images = ["1","2","3","4","5"]
 var i = 0
 class GameView: UIViewController {
 
@@ -18,22 +21,41 @@ class GameView: UIViewController {
     @IBOutlet weak var finish_button: UIButton!
     @IBOutlet weak var time_out: UIImageView!
     @IBOutlet weak var timeLabel: UILabel!
-    
-    @IBOutlet weak var apple: UIButton!
+    var apples: [UIButton] = [UIButton]()
     class Apple{
         var type:Int
-        var left:Int
-        
+        var x:Int
+        var y:Int
         init(){
             self.type = Int.random(in: 0...1)
-            self.left = Int.random(in: 1...360)
+            self.x = ((Int.random(in: 1...300)%3)*100)+10
+            self.y = Int.random(in: 100...200)
         }
     }
     
+    @objc func press(sender: UIButton!)
+    {
+        if sender.currentImage == UIImage(named:"apple-removebg.png"){
+            if let win_score = win.text, let good = Int(win_score){
+                AudioServicesPlaySystemSound(1057)
+                win.text = String(good+1)
+            }
+            
+        }
+        else
+        {
+            if let lose_score = lose.text, let bad = Int(lose_score){
+                AudioServicesPlaySystemSound(1006)
+                lose.text = String(bad+1)
+            }
+            
+        }
+        sender.removeFromSuperview()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var sec = 3
+        var sec = 30
         time_out.isHidden = true
         finish_button.isHidden = true
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (_) in
@@ -42,22 +64,50 @@ class GameView: UIViewController {
             if sec > 0
             {
                 sec = sec - 1
-                UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 3.5, delay: 0, animations: {
-                    self.apple.center = CGPoint(x: Int.random(in: 100..<410), y: 890)
-                })
             }
             else
             {
+                for item in self.apples{
+                    item.isHidden = true
+                }
+                
+                self.time_out.isHidden = false
+                self.finish_button.isHidden = false
+                self.timeLabel.isHidden = true
                 if timer != nil {
                     timer?.invalidate()
-                    self.time_out.isHidden = false
-                    self.finish_button.isHidden = false
-                    self.timeLabel.isHidden = true
+                    gameTimer?.invalidate()
+                    down?.invalidate()
                 }
             }
             
         }
         
+        down = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (_) in
+            for item in self.apples{
+                item.center.y += 10
+            }
+            
+        }
+        
+        gameTimer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { (_) in
+            if Int.random(in:  0...100)%2 == 0
+            {
+                let button = UIButton()
+                let fruit = Apple()
+                if fruit.type == 1{
+                    button.setImage(UIImage(named: "apple-removebg.png"), for: .normal)
+                }
+                else{
+                    button.setImage(UIImage(named: "badapple-removebg.png"), for: .normal)
+                }
+                button.frame = CGRect(x: fruit.x, y:fruit.y, width:123,height:86)
+                button.addTarget(self, action: #selector(self.press(sender:)), for: .touchUpInside)
+                self.apples.append(button)
+                self.view.addSubview(button)
+            }
+            
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- game/ViewController.swift
+++ game/ViewController.swift
@@ -8,20 +8,28 @@
 
 import UIKit
 import SafariServices
+import AVFoundation
 import UserNotifications
 class ViewController: UIViewController , SFSafariViewControllerDelegate {
-
+    
+    var looper: AVPlayerLooper?
     @IBOutlet weak var highestScore: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         let highest = UserDefaults.standard.integer(forKey: "highest")
         highestScore.text = String(highest)
+        if let url = URL(string: "https://r2---sn-ipoxu-un5s.googlevideo.com/videoplayback?id=o-AGEh_tEifByfvO6PUZ6CmQhDaCCM43Tb13UN1Vknyfkx&itag=140&source=youtube&requiressl=yes&mm=31,29&mn=sn-ipoxu-un5s,sn-un57sn76&ms=au,rdu&mv=m&pl=24&ei=FzHYXNXrHoin4ALv7ZSACw&gcr=tw&initcwndbps=1091250&mime=audio/mp4&gir=yes&clen=846015&otfp=1&dur=52.221&lmt=1541860943712796&mt=1557672119&fvip=4&keepalive=yes&c=WEB&txp=2211222&ip=114.35.111.239&ipbits=0&expire=1557693816&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,ei,gcr,initcwndbps,mime,gir,clen,otfp,dur,lmt&signature=08A1FDD6ED1B990DAAD7ABD2EEF4BA80AF050B4D.D91C82E48E471D777DA89E9711ACBC6EDA0AF6C6&key=yt8&signature=undefined&title=%E6%91%A9%E7%88%BE%E8%8E%8A%E5%9C%92-%E9%96%8B%E5%BF%83%E8%BE%B2%E5%A0%B4%E8%83%8C%E6%99%AF%E9%9F%B3%E6%A8%82+%E9%A6%96%E6%92%AD%E6%B8%AC%E8%A9%A6") {
+            let player = AVQueuePlayer()
+            let item = AVPlayerItem(url: url)
+            looper = AVPlayerLooper(player: player, templateItem: item)
+            player.play()
+        }
     }
 
     
     @IBAction func goToWeb(_ sender: AnyObject)
     {
-        if let url = URL(string: "https://medium.com/")
+        if let url = URL(string: "https://medium.com/@apppeterpan")
         {
             let safari = SFSafariViewController(url: url)
             safari.delegate = self
